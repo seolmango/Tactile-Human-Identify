09ae569fddcc7e09699da47907dccd0c91292c83
Seolchaehwan <seolchaehwan@naver.com>
2024-03-10 09:09:02 +0900

    정확도 낮은 경우 확인

---
+++ check_wrong_type.py
@@ -0,0 +1,36 @@
+from tactile.data_loader import DataLoader
+from tactile.classifier import Classifier
+from tactile.cropper import Foot_cropper
+import numpy as np
+from tqdm import tqdm
+from tactile.tools import save_data_visualization
+
+DataLoader = DataLoader(
+    '230823_walkingData',
+    {
+        'sch': ['1']
+    }
+)
+
+Cropper = Foot_cropper(
+    64, 64, 25, 25, 15, 15, 100
+)
+
+Classifier = Classifier('./data_make/model.pth')
+
+label = ['cyh', 'ksh', 'sch']
+wrong_images = []
+for index, keys in enumerate(DataLoader.keys):
+    for frame in tqdm(range(1, DataLoader.get_data_length(keys))):
+        now = DataLoader.get_data(keys, frame)
+        before = DataLoader.get_data(keys, frame - 1)
+        loc = Cropper.crop(now, before)
+        images = Cropper.crop_image(now, loc)
+        for count, img in enumerate(images):
+            result = Classifier.predict(img)
+            if result != 2:
+                wrong_images.append((img, result))
+
+print(len(wrong_images))
+for i, (img, result) in enumerate(wrong_images):
+    save_data_visualization(img, f'./wrong_images/{i}_{label[result]}.png')
